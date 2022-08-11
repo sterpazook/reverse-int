1110b8e2689812e546d247956ed84e8d62fc0064
Aliaxandr Maiseyenka <sterpazook@gmail.com>
2022-08-11 15:08:04 +0300

    feat: add function reverse(n)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let reversedNumber = "";
+    let str = String(n);
+    
+    if (str[0] == '-') {
+        str = str.slice(1);
+    };
+    
+    for (let i = str.length; i > 0; i--) {
+        reversedNumber = reversedNumber + str[i - 1];
+    }
+    return +reversedNumber;
 }
